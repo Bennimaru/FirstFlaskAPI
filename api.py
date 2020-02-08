1af84b78ad9a8795fe3f637444898d0b900a683f
V V <bennywhyu@gmail.com>
2020-02-08 16:17:55 -0500

    local data loaded and basic get request

--- api.py
+++ api.py
@@ -1,15 +1,52 @@
 import flask
-# import the library
+from flask import request, jsonify
+# import the libraries needed
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 # starts the debugger instead of generic error message in the browser
+
+pokemons = [
+    {'id': 0,
+     'name': 'Bulbasaur',
+     'type': 'Grass',
+     'generation': '1'},
+    {'id': 1,
+     'name': 'Charmander',
+     'type': 'Fire',
+     'generation': '1'},
+    {'id': 2,
+     'name': 'Squirtle',
+     'type': 'Water',
+     'generation': '1'}
+]
 
 
 @app.route('/', methods=['GET'])
 # maps the url and the allowed methods to the specified function
 def home():
     return "<h1>From Gem Mining to Snake Charming</h1><p>First foray into building something with Python. If Sinatra is to Flask then 'Sinatra doesn't know this ditty' will be...</p>"
+
+
+@app.route('/api/v1/resources/pokemons/all', methods=['GET'])
+def api_all():
+    return jsonify(pokemons)
+
+
+@app.route('/api/v1/resources/pokemons', methods=['GET'])
+def api_id():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id provided. Please specify an id."
+
+    results = []
+
+    for pokemon in pokemons:
+        if pokemon['id'] == id:
+            results.append(pokemon)
+
+    return jsonify(results)
 
 
 app.run()
